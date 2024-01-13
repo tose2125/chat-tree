b97b30bb0a1f308a24b2aa08c989b7b1129ac692
tose2125 <tose2125@tose2125.net>
2024-01-14 00:30:19 +0900

    Add watch mode

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
+import { watch } from 'chokidar'
 import { loadNodes } from './load-nodes.js'
 import { savePages } from './save-pages.js'
 
-savePages(process.argv[3], loadNodes(process.argv[2]))
+let timeout: NodeJS.Timeout
+watch(process.argv[2]).on('all', () => {
+  clearTimeout(timeout)
+  timeout = setTimeout(() => {
+    console.log(new Date())
+    savePages(process.argv[3], loadNodes(process.argv[2]))
+  }, 1000)
+})
